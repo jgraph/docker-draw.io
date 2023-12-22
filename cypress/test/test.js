92c9f6d35826a791e5b461c32ff3fd70806482d3
a-mankinenko <alexander.mankinenko@codefresh.io>
2023-12-22 17:03:15 +0300

    upd test plan

--- cypress/test/test.js
+++ cypress/test/test.js
@@ -2,7 +2,7 @@ describe('test', () => {
   it('look for a button', () => {
     cy.visit('http://localhost:8080')
 
-    cy.contains('Decide later')
-    cy.contains('More Shapes')
+    cy.contains('Decide later').click()
+    cy.contains('More Shapes').click()
   })
 })
